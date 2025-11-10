48fe209516768a84d34a40cb9c0daf468c000cb1
Jorge Claudio <jorgeclaudiocf@gmail.com>
2025-11-10 11:56:04 -0300

    Exercicio 003 - Sucessor e Antecessor

---
+++ ex003-sucessorAntecessor.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número inteiro: '))
+sucessor = num + 1
+antecessor = num - 1
+
+print(f'O sucessor de {num} é igual a {sucessor} e seu antecessor é igual a {antecessor}')
