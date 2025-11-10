50f2b513dbc9af046b2ddf78011e387520c14e42
Jorge Claudio <jorgeclaudiocf@gmail.com>
2025-11-10 12:03:16 -0300

    Calculo dobro, triplo, raiz quadrada e cubica

---
+++ ex004-dobroTriploRaiz.py
@@ -0,0 +1,7 @@
+num = int(input('Digite um número inteiro: '))
+dobro = num * 2
+triplo = num * 3
+raiz = num ** (1/2)
+raizCubica = num ** (1/3)
+
+print(f'O dobro do número {num} é igual a {dobro}, seu triplo é {triplo}, sua raiz quadrada é {raiz:.3f} e sua raiz cúbica é igual a {raizCubica:.3f}.')
