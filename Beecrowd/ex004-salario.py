ad37117152e76b09cf63d9bcfa103b681aebb892
Jorge Claudio <jorgeclaudiocf@gmail.com>
2025-11-15 18:49:28 -0300

    3 exercicios simples de input e output matematicos

---
+++ Beecrowd/ex004-salario.py
@@ -0,0 +1,6 @@
+num = int(input(""))
+horas = int(input(""))
+valor_hora = float(input(""))
+pagamento = horas * valor_hora
+
+print(f"NUMBER = {num}\nSALARY = U$ {pagamento:.2f}")
---
+++ Beecrowd/ex005-salarioAumento.py
@@ -0,0 +1,6 @@
+nome = str(input(""))
+fixo = float(input(""))
+vendas = float(input(""))
+ganho = fixo + (vendas * 15 / 100)
+
+print(f"TOTAL = R$ {ganho:.2f}")
