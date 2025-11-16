d0d0d9298183bc4cd41e1d2e9efc22db0e218659
Jorge Claudio <jorgeclaudiocf@gmail.com>
2025-11-16 15:40:59 -0300

    2 exercicios de if else

---
+++ Estudo-proprio/modulos/ex003-maiorMenor.py
@@ -0,0 +1,11 @@
+numA = float(input("Digite um número: "))
+numB = float(input("Digite outro número: "))
+
+if numA > numB:
+    print(f"{numA} é maior que {numB}")
+
+elif numA == numB:
+    print(f"{numA} é igual a {numB}")
+
+else:
+    print(f"{numB} é maior que {numA}")
